bb70aaeb877d2d8a938a8453dc688ad84f528a19
Sergei Garcia Ortega <sgarcia.dev@gmail.com>
2018-12-12 20:13:49 +0000

    Changes on client side for Apollo Client integration

--- client/src/requests.js
+++ client/src/requests.js
@@ -1,84 +1,123 @@
+import { ApolloClient, ApolloLink, HttpLink, InMemoryCache } from 'apollo-boost';
+import gql from 'graphql-tag';
 import { isLoggedIn, getAccessToken } from './auth';
 
 const endpointURL = "http://localhost:9000/graphql";
 
+const authLink = new ApolloLink(
+  (operation, forward) => {
+    if (isLoggedIn()) {
+      operation.setContext({
+        headers: {
+          authorization: `Bearer ${getAccessToken()}`
+        }
+      });
+    }
+    return forward(operation);
+  }
+);
+
+const client = new ApolloClient({
+  link: ApolloLink.from([
+    authLink,
+    new HttpLink({
+      uri: endpointURL
+    })
+  ]),
+  cache: new InMemoryCache()
+});
+
+const JobDetailsFragment = gql`
+  fragment JobDetails on Job {
+    id
+    title
+    description
+    company {
+      id
+      name
+    }
+  }
+`;
+
+const createJobMutation = gql`
+  mutation CreateJob($input: CreateJobInput) {
+    job: createJob(input: $input) {
+      ...JobDetails
+    }
+  }
+  ${JobDetailsFragment}`;
+
+const loadJobQuery = gql`
+  query JobQuery($id: ID!) {
+    job(id: $id) {
+      ...JobDetails
+    }
+  }
+  ${JobDetailsFragment}`;
+
+const loadJobsQuery = gql`
+  {
+    jobs {
+      ...JobDetails
+    }
+  }
+  ${JobDetailsFragment}`;
+
+const loadCompanyQuery = gql`
+  query CompanyQuery($id: ID!) {
+    company(id: $id) {
+      id
+      name
+      description
+      jobs {
+        ...JobDetails
+      }
+    }
+  }
+  ${JobDetailsFragment}`;
+
 export async function createJob(input) {
-  const { job } = await graphqlRequest({
-    query: `mutation CreateJob($input: CreateJobInput) {
-      job: createJob(input: $input) {
-        id
-        title
-        description
-        company {
-          id
-          name
-        }
-      }
-    }`,
-    variables: { input }
-  });
-  return job;
-}
-
-export async function loadCompany(id) {
-  const data = await graphqlRequest({
-    query: `query CompanyQuery($id: ID!) {
-      company(id: $id) {
-        id
-        name
-        description
-        jobs {
-          id
-          title
-          description
-          company {
-            id
-            name
-          }
-        }
-      }
-    }`,
-    variables: { id }
-  });
-  return data.company;
-}
-
-export async function loadJob(id) {
-  const data = await graphqlRequest({
-    query: `query JobQuery($id: ID!) {
-      job(id: $id) {
-        id
-        title
-        description
-        company {
-          id
-          name
-        }
-      }
-    }`,
-    variables: { id }
+  const variables = { input };
+  
+  const { data } = await client.mutate({
+    mutation: createJobMutation,
+    variables,
+    update: (cacheStore, mutationResult) => {
+      const {data} = mutationResult;
+      cacheStore.writeQuery({
+        query: loadJobQuery,
+        variables: { id: data.job.id },
+        data
+      })
+    }
   });
   return data.job;
 }
 
-export async function loadJobs() {
-  const data = await graphqlRequest({
-    query: `{
-      jobs {
-        id
-        title
-        company {
-          id
-          name
-        }
-      }
-    }`
-  });
+export async function loadCompany(id) {
+  const variables = { id };
+  
+  const {data} = await client.query({query: loadCompanyQuery, variables});
+  return data.company;
+}
 
+export async function loadJob(id) {
+  const variables = { id };
+
+  const {data} = await client.query({query: loadJobQuery, variables});
+  return data.job;
+}
+
+export async function loadJobs() {
+  const {data} = await client.query({
+    query: loadJobsQuery,
+    fetchPolicy: 'no-cache'
+  });
   return data.jobs;
 }
 
-async function graphqlRequest({ query, variables = {} }) {
+// Apollo-less GraphQL Request
+/* async function graphqlRequest({ query, variables = {} }) {
   const request = {
     method: "POST",
     headers: {
@@ -104,4 +143,4 @@ async function graphqlRequest({ query, variables = {} }) {
   }
 
   return responseBody.data;
-}
+} */
